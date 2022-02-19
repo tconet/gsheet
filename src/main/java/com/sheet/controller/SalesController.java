cce35ed1a75665396c5e4592fccd6d4406f623cc
tconet <tiagonet@gmail.com>
2022-02-19 16:41:55 -0300

    adicionando parametros na chamada de vendas, proteina e a gramatura

--- src/main/java/com/sheet/controller/SalesController.java
+++ src/main/java/com/sheet/controller/SalesController.java
@@ -1,6 +1,7 @@
 package com.sheet.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sheet.domain.Protein;
@@ -8,8 +9,12 @@ import com.sheet.domain.Protein;
 @RestController
 public class SalesController {
 
-	@GetMapping("/sales")
-	public Protein sales() {
+	@GetMapping("/vendas")
+	public Protein sales(
+			@RequestParam("proteina") String protein,
+			@RequestParam("gramatura") int gram) {
+		System.out.println(protein);
+		System.out.println(gram);
 		return new Protein("Frango", 150, 10);
 	}
 }
